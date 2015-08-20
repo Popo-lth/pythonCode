ffd5167b980463c8f59413e83cdad5087a076031
刘天昊 <tianhao.liu@renren-inc.com>
2015-08-20 20:38:17 +0800

    update

--- sort/shellSort.py
+++ sort/shellSort.py
@@ -24,11 +24,23 @@ class Solution:
 
     # @param {integer[]} nums
     def shellSort(self, nums):
-        print 'shellSort'
+        end = len(nums)
+
+        step = end >> 1
+        while step > 0:
+            for i in xrange(step, end, step):
+                if nums[i] < nums[i-step]:
+                    n = nums[i]
+                    j = i - step
+                    while j >= 0 and nums[j] > n:
+                        nums[j+step] = nums[j]
+                        j -= step
+                    nums[j+step] =n
+            step >>= 1
 
     def createArray(self):
         array = []
-        for i in xrange(0, random.randint(5,10)):
+        for i in xrange(0, random.randint(500,1000)):
             array.append(random.randint(0, 100))
         return array
 
@@ -40,23 +52,25 @@ if __name__ == '__main__':
     #a = [29, 69, 53, 31, 75, 55]
     print a
     
-    ac = list(a)
+    aa = list(a)
     start = time.time()
-    s.insertSort(ac)
+    s.insertSort(aa)
     end = time.time()
-    print ac, 
+    #print aa, 
     print '%0.6f' % (end - start)
 
-    ac = list(a)
+    ab = list(a)
     start = time.time()
-    s.shellSort(ac)
+    s.shellSort(ab)
     end = time.time()
-    print ac, 
+    #print ab, 
     print '%0.6f' % (end - start)
     
     ac = list(a)
     start = time.time()
     ac.sort()
     end = time.time()
-    print ac,
+    #print ac,
     print '%0.6f' % (end - start)
+
+    print ab == ac and ab == aa
